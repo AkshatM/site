fc6f3d67eb4b064ffce0d8bc3541bd825e2d91f1
Akshat Mahajan <akshat-mahajanm.bkk@gmail.com>
2025-07-30 10:42:10 -0400

    Fixup: omit the dark mode requirement

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Akshat Mahajan | Distributed Systems @ Cloudflare | Making the Internet a better place';
-export const SITE_DESCRIPTION = 'Akshat Mahajan | Distributed Systems @ Cloudflare | Making the Internet a better place';
+export const SITE_DESCRIPTION = 'The personal webpage of Akshat Mahajan, a systems engineer with a focus on fast Rust and quiet living. Explore my projects, blog posts, and professional journey.';
