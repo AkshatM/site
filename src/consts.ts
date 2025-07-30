f764cd0bacc7576e6351226011ce38782dc8285d
Akshat Mahajan <akshat-mahajanm.bkk@gmail.com>
2025-07-30 03:20:41 -0400

    Rewrite to use Astro and design a winning Tailwind theme

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Akshat Mahajan | Distributed Systems @ Cloudflare | Making the Internet a better place';
+export const SITE_DESCRIPTION = 'Akshat Mahajan | Distributed Systems @ Cloudflare | Making the Internet a better place';
