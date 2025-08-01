59d0c9e5add813db37ce465471d29dedf491d37b
Akshat Mahajan <akshat-mahajanm.bkk@gmail.com>
2025-08-01 18:36:11 -0400

    Add a better self-hosted favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://akshatmahajan.com",
   integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
