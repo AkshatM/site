d35565d2a13d1efd8ff821ac65a76f7984f56af2
Akshat Mahajan <akshat-mahajanm.bkk@gmail.com>
2025-08-12 17:06:29 -0400

    Get a blog page up and running

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,23 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://akshatmahajan.com",
   integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwindcss()] },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-mocha",
+        dark: "catppuccin-latte",
+      },
+      wrap: false,
+    },
+  },
 });
