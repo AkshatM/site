8483cf87f80e2e2a5e80239f37aa1fae90da1700
Akshat Mahajan <akshat-mahajanm.bkk@gmail.com>
2025-08-13 19:52:13 -0400

    Finalize blog and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,16 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import remarkGfm from "remark-gfm";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://akshatmahajan.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [expressiveCode(), mdx(), sitemap()],
   vite: { plugins: [tailwindcss()] },
   markdown: {
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkGfm, remarkMath],
     rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
