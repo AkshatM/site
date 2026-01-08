1d59ca5697370487ae9a50408bbad902b0b8340d
Akshat Mahajan <akshatm.bkk@gmail.com>
2026-01-08 11:37:00 -0500

    Fix some colors and designs

--- src/components/Upvote.tsx
+++ src/components/Upvote.tsx
@@ -49,13 +49,17 @@ export default function LikeButton({ postId }: { postId: string }) {
 
   const commonState = `inline-flex items-center gap-x-1 rounded-lg border border-border px-2 py-1 transition-all rounded-xl`
   const preClicked = `${commonState} bg-primary-foreground active:scale-95 hover:bg-input`
-  const clicked = `${commonState} hover:bg-input bg-input`
+  const clicked = `${commonState} hover:none bg-amber-300 text-black`
 
   return (
     disabled ? 
     <p><i>Upvoting temporarily disabled</i></p> : 
-    <button onClick={handleLikeClick} className={liked ? clicked : preClicked} disabled={liked}>
-      {liked ? '❤️' : '🤍'} {likes}
+    <button onClick={handleLikeClick} className={liked ? clicked : preClicked} disabled={liked} title="Toast this post">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <polyline points="17 11 12 6 7 11"></polyline>
+            <polyline points="17 18 12 13 7 18"></polyline>
+        </svg>
+        <small>{likes}</small>
     </button>
   );
 }
